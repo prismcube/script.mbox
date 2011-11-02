8c5ec84eafe722b17cfec331b677a2187dd0f296
bsyoun <bsyoun@marusys.com>
2011-11-02 18:39:50 +0900

    add func: progress(), gmtime

--- pvr/gui/windows/channelbanner.py
+++ pvr/gui/windows/channelbanner.py
@@ -27,8 +27,11 @@ from pvr.gui.basewindow import Action
 from inspect import currentframe
 
 import pvr.elismgr
+from pvr.net.net import EventRequest
 #from pvr.net.net import EventServer, EventHandler, EventRequest
 
+#from threading import Thread
+import threading
 import logging
 
 log = logging.getLogger('mythbox.ui')
@@ -38,7 +41,7 @@ mlog = logging.getLogger('mythbox.method')
 class ChannelBanner(BaseWindow):
 	def __init__(self, *args, **kwargs):
 		BaseWindow.__init__(self, *args, **kwargs)
-		print 'f_lineno[%d] co_filename[%s]' %(currentframe().f_lineno, currentframe().f_code.co_filename)    
+		print 'f_coname[%s] f_lineno[%d] co_filename[%s]' %(currentframe().f_code.co_name, currentframe().f_lineno, currentframe().f_code.co_filename)    
 		print 'args[0]=[%s]' % args[0]
 		print 'args[1]=[%s]' % args[1]
 
@@ -51,12 +54,27 @@ class ChannelBanner(BaseWindow):
 
 		#push push test test
 
+		#time track
+		#from threading import Thread
+		self.timeTrack = threading.Thread(target = self.func_Progress)
+		self.untilThread = True
+
+	def __del__(self):
+		print '[%s():%s] destroyed ChannelBanner'% (currentframe().f_code.co_name, currentframe().f_lineno)
+
+		# end thread func_progress()
+		self.untilThread = False
+
 	def onInit(self):
-		print 'ChannelBanner #0'	
+		print '[%s():%s]'% (currentframe().f_code.co_name, currentframe().f_lineno)
 		if not self.win:
 			self.win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
 
+		#get channel
 		self.currentChannel = self.commander.channel_GetCurrent()
+
+		#get event
+		#request = EventRequest(self)
 
 		self.ctrlChannelNumber = self.getControl( 600 )
 		self.ctrlChannelName = self.getControl( 601 )
@@ -64,10 +82,18 @@ class ChannelBanner(BaseWindow):
 		self.ctrlEventStartTime = self.getControl( 704 )
 		self.ctrlEventEndTime = self.getControl( 705 )
 
-		print 'ChannelBanner #1'
+		self.ctrlProgress = self.getControl(707)
+		#self.ctrlProgress = xbmcgui.ControlProgress(100, 250, 125, 75)
+		#self.ctrlProgress(self.Progress)
+		self.ctrlProgress.setPercent(0)
+		self.progress_idx = 0
+		self.progress_max = 0
+		
+
+		print 'currentChannel[%s]' % self.currentChannel
 		if( self.currentChannel[0] == 'NULL' ) :
 			print 'has no channel'
-			print 'ChannelBanner #2'			
+		
 			# todo 
 			# show message box : has no channnel
 		else :
@@ -77,8 +103,21 @@ class ChannelBanner(BaseWindow):
 			self.ctrlEventName.setLabel('')
 			self.ctrlEventStartTime.setLabel('00:00')
 			self.ctrlEventEndTime.setLabel('00:00')
-			
 
+			print '[%s():%s]start thread, func_progress()'% (currentframe().f_code.co_name, currentframe().f_lineno)
+			self.timeTrack.start()
+
+		"""
+		stbTime_GMT    = self.commander.datetime_GetGMTTime()
+		stbTime_offset = self.commander.datetime_GetLocalOffset()
+		stbTime_local  = self.commander.datetime_GetLocalTime()
+		print 'GMT    time[%s]' % stbTime_GMT
+		print 'offset time[%s]' % stbTime_offset
+		print 'local  time[%s]' % stbTime_local
+		print '%s' % time.strftime('%a, %d.%m.%Y %I:%M', time.gmtime(int(stbTime_GMT[0])) )
+		print '%s' % time.strftime('%a, %d.%m.%Y %I:%M', time.gmtime(int(stbTime_offset[0])) )
+		print '%s' % time.strftime('%a, %d.%m.%Y %I:%M', time.gmtime(int(stbTime_local[0])) )
+		"""
 
 	def onAction(self, action):
 		id = action.getId()
@@ -90,18 +129,23 @@ class ChannelBanner(BaseWindow):
 			print '<<<<< test youn: ID[%s]' % id
 			log.debug('youn:%s' % id)
 
-			
+	
 		elif id == Action.ACTION_PARENT_DIR:
 			print 'youn check ation back'
 			winmgr.getInstance().showWindow( winmgr.WIN_ID_CHANNEL_LIST_WINDOW )
 #			winmgr.getInstance().showWindow( winmgr.WIN_ID_NULLWINDOW )
 #			winmgr.shutdown()
 
+			# end thread func_progress()
+			self.untilThread = False
+
 		else:
 			print 'youn check action unknown id=%d' % id
 
+
 	def onClick(self, controlId):
 		print "onclick(): control %d" % controlId
+		
 
 	def onFocus(self, controlId):
 		print "onFocus(): control %d" % controlId
@@ -117,23 +161,51 @@ class ChannelBanner(BaseWindow):
 		if not event[6]: pass
 		else:
 			print 'event6[%s] event7[%s]'% (event[6], event[7])
-			self.fun_timeData(int(event[6]), int(event[7]))
+			self.func_timeData(int(event[6]), int(event[7]))
+			self.progress_max = int(event[7])
 	
 
-	def fun_timeData(self, startTime, duration):
-			print '<<<<<<<< startTime[%s] duration[%s]'% (startTime, duration)
-			timezone_sec = 8 * 3600
-			
-			startTime_hh = time.strftime('%H', time.localtime(startTime - timezone_sec))
-			startTime_mm = time.strftime('%M', time.localtime(startTime - timezone_sec))
-			endTime_hh = time.strftime('%H', time.localtime((startTime+duration) - timezone_sec))
-			endTime_mm = time.strftime('%M', time.localtime((startTime+duration) - timezone_sec))
+	def func_timeData(self, startTime, duration):
+		print '<<<<<<<< startTime[%s] duration[%s]'% (startTime, duration)
 
-			str_startTime = str ('%02s:%02s'% (startTime_hh,startTime_mm) )
-			str_endTime = str ('%02s:%02s'% (endTime_hh,endTime_mm) )
+		# How about slowly time(West) at localoffset...? 
+		timezone_sec = self.commander.datetime_GetLocalOffset()
 
-			self.ctrlEventStartTime.setLabel(str_startTime)
-			self.ctrlEventEndTime.setLabel(str_endTime)
+		startTime_hh = time.strftime('%H', time.gmtime(startTime + int(timezone_sec[0])) )
+		startTime_mm = time.strftime('%M', time.gmtime(startTime + int(timezone_sec[0])) )
+		endTime_hh = time.strftime('%H', time.gmtime((startTime+duration) + int(timezone_sec[0])) )
+		endTime_mm = time.strftime('%M', time.gmtime((startTime+duration) + int(timezone_sec[0])) )
 
+		str_startTime = str ('%02s:%02s'% (startTime_hh,startTime_mm) )
+		str_endTime = str ('%02s:%02s'% (endTime_hh,endTime_mm) )
+
+		self.ctrlEventStartTime.setLabel(str_startTime)
+		self.ctrlEventEndTime.setLabel(str_endTime)
+
+	def func_Progress(self):
+		print '[%s():%s] <<<< begin'% (currentframe().f_code.co_name, currentframe().f_lineno)
+		while self.untilThread:
+			#select.select([self.progress_start], [], [], 0.5 )
+#			if self.progress_max:
+
+			print 'progress_max[%s]'% self.progress_max
+
+
+			print '[%s():%s]'% (currentframe().f_code.co_name, currentframe().f_lineno)
+			print self.ctrlProgress.getPercent()
+			print '[%s():%s]'% (currentframe().f_code.co_name, currentframe().f_lineno)
+
+			print 'progress_idx[%s]' % self.progress_idx
+			self.ctrlProgress.setPercent(self.progress_idx)
+
+			self.progress_idx += 10  #(self.progress_max / 100)
+			if self.progress_idx > 100:
+				self.progress_idx = 100
+
+
+			time.sleep(10)
+			print '[%s():%s]'% (currentframe().f_code.co_name, currentframe().f_lineno)
+
+		
 
 
