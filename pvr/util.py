2a9f1d236cfe6abffacc2c0d950564bfa7b98ead
bsyoun <bsyoun@marusys.com>
2011-11-08 13:19:44 +0900

    re up util.py

--- pvr/util.py
+++ pvr/util.py
@@ -7,6 +7,7 @@ from decorator import decorator
 from odict import odict
 from threading import RLock
 
+from inspect import currentframe
 
 __workersByName = odict()
 
@@ -82,5 +83,82 @@ def is_digit(str):
 		return True
 	except ValueError:
 		return False
+
+@decorator
+def synchronized(func):
+	"""Synchronizes method invocation on an object using the method name as the mutex"""
+	
+	def wrapper(self,*__args,**__kw):
+		try:
+			rlock = self.__get__('_sync_lock_%s' % func.__name__)
+			#rlock = self._sync_lock
+		except AttributeError:
+			from threading import RLock
+			rlock = self.__dict__.setdefault('_sync_lock_%s' % func.__name__, RLock())
+		rlock.acquire()
+		try:
+			return func(self,*__args,**__kw)
+		finally:
+			rlock.release()
+
+	wrapper.__name__ = func.__name__
+	wrapper.__dict__ = func.__dict__
+	wrapper.__doc__ = func.__doc__
+	return wrapper
+
+@decorator
+def sync_instance(func):
+	"""Synchronizes method invocation on an object using the object instance as the mutex"""
+	
+	def wrapper(self,*__args,**__kw):
+		try:
+			rlock = self._sync_lock
+		except AttributeError:
+			from threading import RLock
+			rlock = self.__dict__.setdefault('_sync_lock', RLock())
+		rlock.acquire()
+		try:
+			return func(self,*__args,**__kw)
+		finally:
+			rlock.release()
+	        
+	wrapper.__name__ = func.__name__
+	wrapper.__dict__ = func.__dict__
+	wrapper.__doc__ = func.__doc__
+	return wrapper
+
+
+import threading
+import thread
+
+
+
+class Mutex(threading.Thread):
+	def __init__(self, condition):
+		threading.Thread.__init__(self)
+		self.condition = condition
+
+		self.mutex = thread.allocate_lock()
+		#self.mutex = threading.RLock()
+		#self.notify = threading.Condition()
+
+	def lock(self):
+		print '[%s():%s]mutex_lock'% (currentframe().f_code.co_name, currentframe().f_lineno)
+		self.mutex.acquire()
+		#self.notify.acquire()
+
+	def noti(self):
+		print '[%s():%s]mutex_notify'% (currentframe().f_code.co_name, currentframe().f_lineno)
+		self.notify.notify()
+
+	def unlock(self):
+		print '[%s():%s]mutex_unlock'% (currentframe().f_code.co_name, currentframe().f_lineno)
+
+#		if self.mutex.locked():
+		self.mutex.release()
+
+		#self.notify.release()
+
+
 
 
