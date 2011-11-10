829ffb7a54ffcc3552ac7261eddd974db2bd4046
donghwa kim <donghwakim@marusys.com>
2011-11-10 19:10:13 +0900

    edit language setting menu

--- pvr/gui/windows/languagesetting.py
+++ pvr/gui/windows/languagesetting.py
@@ -16,34 +16,52 @@ class LanguageSetting(BaseWindow):
 		self.commander = pvr.elismgr.getInstance().getCommander()
 
 		# Description List
-		self.DescriptionList				= ['Set menu and popup language', 'Set primary audio language', 'Set primary subtitle language', 'Set secondary subtitle language', 'Enable hearing impaired support']
+		self.DescriptionList					= ['Set menu and popup language', 'Set primary audio language', 'Set primary subtitle language', 'Set secondary subtitle language', 'Enable hearing impaired support']
 
 		# OSD Language
-		self.osdlangugelist 				= []
-		self.osdLanguageProperty			= ElisPropertyEnum( 'Language' )
-		self.count							= self.osdLanguageProperty.getIndexCount()
+		self.osdlangugelist 					= []
+		self.osdLanguageProperty				= ElisPropertyEnum( 'Language' )
 		
-		for i in range( self.count ):
+		for i in range( self.osdLanguageProperty.getIndexCount() ):
 			listItem = xbmcgui.ListItem(self.osdLanguageProperty.getName(), self.osdLanguageProperty.getPropStringByIndex( i ),"-", "-", "-")
 			self.osdlangugelist.append(listItem)
 
 		# Primary Audio Language
-		self.primaryAudioLanguagelist 		= []
-		self.primaryAudioLanguageProperty	= ElisPropertyEnum( 'Audio Language' )
-		self.count							= self.primaryAudioLanguageProperty.getIndexCount()
-		
-		for i in range( self.count ):
+		self.primaryAudioLanguagelist 			= []
+		self.primaryAudioLanguageProperty		= ElisPropertyEnum( 'Audio Language' )
+
+		for i in range( self.primaryAudioLanguageProperty.getIndexCount() ):
 			listItem = xbmcgui.ListItem(self.primaryAudioLanguageProperty.getName(), self.primaryAudioLanguageProperty.getPropStringByIndex( i ),"-", "-", "-")
 			self.primaryAudioLanguagelist.append(listItem)
+
+		# Primary Subtitle Language
+		self.primarySubtitleLanguagelist 		= []
+		self.primarySubtitleLanguageProperty	= ElisPropertyEnum( 'Subtitle Language' )
+		
+		for i in range( self.primarySubtitleLanguageProperty.getIndexCount() ):
+			listItem = xbmcgui.ListItem(self.primarySubtitleLanguageProperty.getName(), self.primarySubtitleLanguageProperty.getPropStringByIndex( i ),"-", "-", "-")
+			self.primarySubtitleLanguagelist.append(listItem)
+
+		# Secondary Subtitle Language
+		self.secondarySubtitleLanguagelist 		= []
+		self.secondarySubtitleLanguageProperty	= ElisPropertyEnum( 'Secondary Subtitle Language' )
+		
+		for i in range( self.secondarySubtitleLanguageProperty.getIndexCount() ):
+			listItem = xbmcgui.ListItem(self.secondarySubtitleLanguageProperty.getName(), self.secondarySubtitleLanguageProperty.getPropStringByIndex( i ),"-", "-", "-")
+			self.secondarySubtitleLanguagelist.append(listItem)
+
+		# For The Hearing Impaired
+		self.forTheHearingImpairedlist		 	= []
+		self.forTheHearingImpairedProperty		= ElisPropertyEnum( 'Hearing Impaired' )
+		
+		for i in range( self.forTheHearingImpairedProperty.getIndexCount() ):
+			listItem = xbmcgui.ListItem(self.forTheHearingImpairedProperty.getName(), self.forTheHearingImpairedProperty.getPropStringByIndex( i ),"-", "-", "-")
+			self.forTheHearingImpairedlist.append(listItem)
 
 
 	def onInit(self):
 		if not self.win:
 			self.win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
-
-		# Description Label
-		self.ctrlDescriptionLabel			= self.getControl( 100 )
-		self.ctrlDescriptionLabel.setLabel( 'Set Menu and popup language' )
 
 		# OSD Language
 		self.ctrlOSDLanguage				= self.getControl( 9100 )
@@ -54,6 +72,27 @@ class LanguageSetting(BaseWindow):
 		self.ctrlPrimaryAudioLanguage		= self.getControl( 9101 )
 		self.ctrlPrimaryAudioLanguage.addItems( self.primaryAudioLanguagelist )
 		self.ctrlPrimaryAudioLanguage.selectItem( self.primaryAudioLanguageProperty.getProp() )
+
+		# Primary Audio Language
+		self.ctrlPrimarySubtitleLanguage	= self.getControl( 9102 )
+		self.ctrlPrimarySubtitleLanguage.addItems( self.primarySubtitleLanguagelist )
+		self.ctrlPrimarySubtitleLanguage.selectItem( self.primarySubtitleLanguageProperty.getProp() )
+
+		# Secondary Subtitle Language
+		self.ctrlSecondarySubtitleLanguage	= self.getControl( 9103 )
+		self.ctrlSecondarySubtitleLanguage.addItems( self.secondarySubtitleLanguagelist )
+		self.ctrlSecondarySubtitleLanguage.selectItem( self.secondarySubtitleLanguageProperty.getProp() )
+
+		# For The Hearing Impaired
+		self.ctrlForTheHearingImpaired		= self.getControl( 9104 )
+		self.ctrlForTheHearingImpaired.addItems( self.forTheHearingImpairedlist )
+		self.ctrlForTheHearingImpaired.selectItem( self.forTheHearingImpairedProperty.getProp() )
+
+		# Description Label
+		self.ctrlDescriptionLabel			= self.getControl( 100 )
+		self.ctrlDescriptionLabel.setLabel( self.DescriptionList[int((self.getFocusid() / 100) - 3)] )
+		
+		
 		'''
 		self.ctrlOSDLanguage    			= self.getControl( 101 )
 		self.ctrlPrimaryAudioLanguage    	= self.getControl( 102 )
@@ -87,9 +126,8 @@ class LanguageSetting(BaseWindow):
 	def onClick(self, controlId):
 		if controlId == 304 or controlId == 305:
 			self.osdLanguageProperty.setPropString( self, self.ctrlOSDLanguagelist.getSelectedItem().getLabel() )
-			print "selected Itemnum of OSD Language = %s" % self.ctrlOSDLanguagelist.getSelectedItem().getLabel()
+		# other setProperty setting here...
 
 	def onFocus(self, controlId):
-		print "onFocus(): control %d" % controlId
-
-
+		# change description
+		self.ctrlDescriptionLabel.setLabel( self.DescriptionList[int(controlId / 100 - 3)] )
