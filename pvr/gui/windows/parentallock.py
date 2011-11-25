e6e0b115eb244d8b81273f311e9a9d96a92e58d9
donghwa kim <donghwakim@marusys.com>
2011-11-25 19:08:52 +0900

    Setting Window Test Code

--- pvr/gui/windows/parentallock.py
+++ pvr/gui/windows/parentallock.py
@@ -10,7 +10,6 @@ import pvr.elismgr
 from pvr.elisproperty import ElisPropertyEnum, ElisPropertyInt
 from pvr.gui.guiconfig import FooterMask
 
-
 E_OSDLanguage					= 1100
 E_PrimaryAudioLanguage			= 1200
 E_PrimarySubtitleLanguage		= 1300
@@ -25,21 +24,27 @@ E_PostRecordingTime				= 1400
 class ParentalLock(SettingWindow):
 	def __init__( self, *args, **kwargs ):
 		SettingWindow.__init__( self, *args, **kwargs)
-		self.commander = pvr.elismgr.getInstance( ).getCommander( )
+		self.commander = pvr.elismgr.getInstance().getCommander( )
 
+		# Description List
 		self.LeftGroupItems						= ['Language', 'Parental']
 		self.navigationIds 						= [E_OSDLanguage,E_PrimaryAudioLanguage,E_PrimarySubtitleLanguage,E_SecondarySubtitleLanguage,E_ForTheHearingImpaired]
 		self.descriptionList					= ['Set menu and popup language', 'Set primary audio language', 'Set primary subtitle language', 'Set secondary subtitle language', 'Enable hearing impaired support']
 	
 		self.ctrlLeftGroup = 0
-		self.groupItems = []		
+		self.groupItems = []
 		
 	def onInit(self):
 		self.win = xbmcgui.Window( xbmcgui.getCurrentWindowId( ) )
 
+		self.setHeaderLabel( self.win, 'Language Preference' )
+		self.setFooter( self.win, ( FooterMask.G_FOOTER_ICON_BACK_MASK | FooterMask.G_FOOTER_ICON_SEARCH_MASK | FooterMask.G_FOOTER_ICON_OK_MASK | FooterMask.G_FOOTER_ICON_RECORD_MASK ) )
+		
 		self.ctrlLeftGroup = self.getControl( 9000 )
 
-		for i in range( len( self.LeftGroupItems ) ):
+		count = len( self.LeftGroupItems )
+			
+		for i in range( count ) :
 			self.groupItems.append( xbmcgui.ListItem( self.LeftGroupItems[i], '-', '-', '-', '-' ) )	
 		
 		self.ctrlLeftGroup.addItems( self.groupItems )
@@ -49,7 +54,6 @@ class ParentalLock(SettingWindow):
 
 		self.setListControl( )
 		
-
 	def onAction( self, action ):
 
 		actionId = action.getId( )
@@ -61,6 +65,7 @@ class ParentalLock(SettingWindow):
 			self.controlSelect( )
 				
 		elif actionId == Action.ACTION_PARENT_DIR :
+			self.close( )
 			winmgr.getInstance().showWindow( winmgr.WIN_ID_MAINMENU )
 
 		elif actionId == Action.ACTION_MOVE_UP :
@@ -68,39 +73,44 @@ class ParentalLock(SettingWindow):
 				self.controlUp( )
 
 		elif actionId == Action.ACTION_MOVE_DOWN :
-			if not( self.getFocusId( ) == 9000 ) :
+			print 'dhkim 1'
+			if self.getFocusId( ) == 9000  :
+				print 'dhkim 2'
+				self.setListControl( )
+			else :
 				self.controlDown( )
 
 		elif actionId == Action.ACTION_MOVE_LEFT :
 			self.setFocus( self.ctrlLeftGroup )
 
-
 	def onClick( self, controlId ):
 		self.controlSelect( )
 		
-		
 	def onFocus( self, controlId ):
-		if self.getFocusId( ) == 9000 :
-			self.setListControl( )
-		#self.controlDescription( self.win, controlId )
 		print 'LanguageSetting test in Focus event id=%d' %controlId
+		#if controlId == 9000 :
+			#print 'dhkim test 9000'
+			#self.setListControl( )
+		#self.controlDescription( self.win, controlId )
+		
 
 	def setListControl( self ):
+		print 'dhkim 0setListControlsetListControlsetListControl'
 		self.removeAllControl( )
 		print 'dhkim test contro numm % d' % self.ctrlLeftGroup.getSelectedPosition( )
 		
-		if self.ctrlLeftGroup.getSelectedPosition( ) == 1 :
+		if self.ctrlLeftGroup.getSelectedPosition( ) == 0 :
+			print 'dhkim test Position'
 			self.addEnumControl( E_OSDLanguage, 'Language' )
 			self.addEnumControl( E_PrimaryAudioLanguage, 'Audio Language' )
 			self.addEnumControl( E_PrimarySubtitleLanguage, 'Subtitle Language' )
 			self.addEnumControl( E_SecondarySubtitleLanguage, 'Secondary Subtitle Language' )
-			self.addEnumControl( E_ForTheHearingImpaired, 'Hearing Impaired' )			
-		elif self.ctrlLeftGroup.getSelectedPosition( ) == 2 :
+			self.addEnumControl( E_ForTheHearingImpaired, 'Hearing Impaired' )
+			
+		elif self.ctrlLeftGroup.getSelectedPosition( ) == 1 :
 			self.addEnumControl( E_AutomaticTimeshift, 'Automatic Timeshift' )
 			self.addEnumControl( E_DefaultRecordDuration, 'Default Rec Duration' )
 			self.addEnumControl( E_PreRecordingTime, 'Pre-Rec Time' )
 			self.addEnumControl( E_PostRecordingTime, 'Post-Rec Time' )
 
 		self.initControl( )
-
-		
