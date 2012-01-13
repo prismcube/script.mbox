7e44305b6416b5ee792301618948ab1bc0e5da43
donghwa kim <donghwakim@marusys.com>
2012-01-13 22:27:32 +0900

    ADD : system info ADD : Installation Main menu

---
+++ pvr/gui/windows/SystemInfo.py
@@ -0,0 +1,94 @@
+"""
+import xbmc
+import xbmcgui
+import sys
+"""
+#import pvr.gui.WindowMgr as WinMgr
+from pvr.gui.BaseWindow import SettingWindow, Action
+#import pvr.ElisMgr
+#from ElisProperty import ElisPropertyEnum
+from pvr.gui.GuiConfig import *
+
+
+class SystemInfo( SettingWindow ) :
+	def __init__( self, *args, **kwargs ) :
+		SettingWindow.__init__( self, *args, **kwargs )
+ 
+		leftGroupItems			= [ 'STB Infomation' ]
+	
+		self.mCtrlLeftGroup 	= None
+		self.mGroupItems 		= []
+		self.mInitialized 		= False
+		self.mLastFocused 		= E_SUBMENU_LIST_ID
+		self.mPrevListItemID 	= 0
+
+		for i in range( len( leftGroupItems ) ) :
+			self.mGroupItems.append( xbmcgui.ListItem( leftGroupItems[i] ) )
+			
+	def onInit( self )  :
+		self.mWinId = xbmcgui.getCurrentWindowId( )
+		self.mWin = xbmcgui.Window( self.mWinId )
+
+		self.mCtrlLeftGroup = self.getControl( E_SUBMENU_LIST_ID )
+		self.mCtrlLeftGroup.addItems( self.mGroupItems )
+
+		position = self.mCtrlLeftGroup.getSelectedPosition( )
+		self.mCtrlLeftGroup.selectItem( position )
+		self.SetListControl( )
+		self.mInitialized = True
+
+	def onAction( self, aAction ) :
+
+		actionId = aAction.getId( )
+		focusId = self.getFocusId( )
+
+		if actionId == Action.ACTION_PREVIOUS_MENU :
+			pass
+			
+		elif actionId == Action.ACTION_SELECT_ITEM :
+			pass
+				
+		elif actionId == Action.ACTION_PARENT_DIR :
+			self.mInitialized = False
+			self.close( )
+
+		elif actionId == Action.ACTION_MOVE_UP :
+			if focusId == E_SUBMENU_LIST_ID and self.mCtrlLeftGroup.getSelectedPosition() != self.mPrevListItemID :
+				self.mPrevListItemID = self.mCtrlLeftGroup.getSelectedPosition( )
+				self.SetListControl( )
+	
+		elif actionId == Action.ACTION_MOVE_DOWN :
+			if focusId == E_SUBMENU_LIST_ID and self.mCtrlLeftGroup.getSelectedPosition() != self.mPrevListItemID :
+				self.mPrevListItemID = self.mCtrlLeftGroup.getSelectedPosition( )
+				self.SetListControl( )
+
+		elif actionId == Action.ACTION_MOVE_LEFT :
+			if focusId != E_SUBMENU_LIST_ID and ( ( focusId % 10 ) == 1 ) :
+				self.setFocusId( E_SUBMENU_LIST_ID )
+				
+		elif actionId == Action.ACTION_MOVE_RIGHT :
+			if focusId == E_SUBMENU_LIST_ID :
+				self.setFocusId( E_SETUPMENU_GROUP_ID )
+
+
+	def onClick( self, aControlId ) :
+		pass
+		
+	def onFocus( self, aControlId ) :
+		if self.mInitialized == False :
+			return
+		if ( self.mLastFocused != aControlId ) or ( self.mCtrlLeftGroup.getSelectedPosition( ) != self.mPrevListItemID ) :
+			if aControlId == E_SUBMENU_LIST_ID :
+				self.SetListControl( )
+				if self.mLastFocused != aControlId :
+					self.mLastFocused = aControlId
+				if self.mCtrlLeftGroup.getSelectedPosition( ) != self.mPrevListItemID :
+					self.mPrevListItemID = self.mCtrlLeftGroup.getSelectedPosition( )
+		
+
+	def SetListControl( self ) :
+		#self.ResetAllControl( )
+		selectedId = self.mCtrlLeftGroup.getSelectedPosition( )
+		
+		if selectedId == 0 :
+			pass
