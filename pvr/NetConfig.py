eb4db53b223f82294b08d03f81864b96f8b7b935
hjkim <hjkim@marusys.com>
2012-02-06 14:08:05 +0900

    none

--- pvr/NetConfig.py
+++ pvr/NetConfig.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-targetIp			= '192.168.101.67'
-myIp				= '192.168.101.69'
-=======
 targetIp			= '127.0.0.1'
 myIp				= '127.0.0.1'
->>>>>>> f5e5cc49e42def57a12a8fd78d1d94d6a7d4a0a9
 
 receiverPort 		= 54321
 commanderPort		= 12345
