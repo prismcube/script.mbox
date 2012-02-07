3303e748b15da148927c3c8d81f323cb390bfa88
donghwa kim <donghwakim@marusys.com>
2012-02-07 18:13:21 +0900

    ADD : base dialog import action class

--- pvr/gui/BaseDialog.py
+++ pvr/gui/BaseDialog.py
@@ -7,6 +7,7 @@ from pvr.gui.BaseWindow import Property
 from ElisProperty import ElisPropertyEnum, ElisPropertyInt
 import pvr.ElisMgr
 from pvr.gui.GuiConfig import *
+from pvr.gui.BaseWindow import Action
 from pvr.Util import RunThread, GuiLock, GuiLock2, MLOG, LOG_WARN, LOG_TRACE, LOG_ERR
 
 class BaseDialog( xbmcgui.WindowXMLDialog, Property ):
