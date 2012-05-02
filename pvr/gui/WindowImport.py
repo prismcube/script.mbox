8b4941b121621318150518fdc322325e70fe3fd3
bsyoun <bsyoun@marusys.com>
2012-05-02 14:11:09 +0900

    debug - import all in GuiHelper

--- pvr/gui/WindowImport.py
+++ pvr/gui/WindowImport.py
@@ -4,6 +4,6 @@ from pvr.gui.BaseWindow import BaseWindow, SettingWindow, Action
 from pvr.gui.BaseDialog import BaseDialog, SettingDialog
 from ElisProperty import ElisPropertyEnum, ElisPropertyInt
 from pvr.Util import RunThread, GuiLock, GuiLock2, TimeToString, TimeFormatEnum
-from pvr.GuiHelper import MR_LANG
+from pvr.GuiHelper import *
 from pvr.gui.GuiConfig import *
 from ElisEventClass import *
