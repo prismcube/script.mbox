ae2a69228d1a81418ffd69e3c460eb082374de1f
donghwa kim <donghwakim@marusys.com>
2012-07-10 00:30:23 +0900

    edit test program UpdateVolume in RootWindow

--- pvr/HiddenTestMgr.py
+++ pvr/HiddenTestMgr.py
@@ -83,17 +83,27 @@ class WaitEventSuite( TestSuite ) :
 
 	def DoCommand( self ) :
 		self.PrintStartTime( )
+		LOG_TRACE( '' )
+		otrinfo = self.mDataCache.Timer_GetOTRInfo( )
+		LOG_TRACE('====================================')
+		LOG_TRACE('====================================')
+		otrinfo.printdebug( )
+		LOG_TRACE('====================================')
+		LOG_TRACE('====================================')
 		self.mEventBus.Register( self )
 		LOG_TRACE( 'Start Wait Event !!!! ' )
 		while self.mEndEvent == False :
+			print 'dhkim test wait event.....'
 			time.sleep(1)
 		self.mEventBus.Deregister( self )
 		LOG_TRACE( 'End Wait Event !!!! ' )
+		otrinfo.printdebug( )
 
 
 	@classmethod
 	def GetName( cls ) :
 		return cls.__name__
+
 
 	def onEvent( self, aEvent ) :
 		if aEvent.getName( ) == self.mEventName :
@@ -101,7 +111,7 @@ class WaitEventSuite( TestSuite ) :
 
 
 	def MappingEventName( self, aValue ) :
-		if aValue == 'stoprecord' :a
+		if aValue == 'stoprecord' :
 			print 'dhkim test event name mapping record'
 			return ElisEventRecordingStopped.getName( )
 
