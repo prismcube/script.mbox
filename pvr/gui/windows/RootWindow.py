392906ceea5111efb8b04b95b746107b1b3f8898
donghwa kim <donghwakim@marusys.com>
2012-08-31 11:29:11 +0900

    cording rule

--- pvr/gui/windows/RootWindow.py
+++ pvr/gui/windows/RootWindow.py
@@ -81,7 +81,7 @@ class RootWindow( BaseWindow ) :
 				WinMgr.GetInstance( ).GetWindow( WinMgr.GetInstance( ).mLastId ).setProperty( 'Signal', 'True' )
 				self.mDataCache.SetLockedState( ElisEnum.E_CC_SUCCESS )
 
-			if WinMgr.GetInstance( ).GetLastWindowID() == WinMgr.WIN_ID_NULLWINDOW :
+			if WinMgr.GetInstance( ).GetLastWindowID( ) == WinMgr.WIN_ID_NULLWINDOW :
 				WinMgr.GetInstance( ).ShowWindow( WinMgr.WIN_ID_NULLWINDOW )
 
 
