ccab905244656eb3a1ff591638a23e433c0fbcc7
lael98 <lael98@localhost.localdomain>
2012-11-30 15:40:09 +0900

    support webinterface

--- default.py
+++ default.py
@@ -5,7 +5,8 @@ import os, sys, xbmcaddon, shutil
 if __name__ == '__main__' :
 	scriptDir = xbmcaddon.Addon( 'script.mbox' ).getAddonInfo( 'path' )
 	sys.path.append( os.path.join( scriptDir, 'pvr' ) )
-	sys.path.append( os.path.join( scriptDir, 'resources' ) )	
+	sys.path.append( os.path.join( scriptDir, 'resources' ) )
+	sys.path.append( os.path.join( scriptDir, 'webinterface' ) )		
 
 	elisDir = xbmcaddon.Addon( 'script.module.elisinterface' ).getAddonInfo( 'path' )
 	sys.path.append( os.path.join( elisDir, 'lib', 'elisinterface' ) )
