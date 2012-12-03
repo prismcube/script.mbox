5d0009ae6aacc086c619bd9310266908fc5503a6
daniel <danielchang@marusys.com>
2012-12-03 14:44:35 +0900

    added & changed desc
    updated mr_lang

--- pvr/gui/windows/FavoriteAddons.py
+++ pvr/gui/windows/FavoriteAddons.py
@@ -161,9 +161,9 @@ class FavoriteAddons( BaseWindow ) :
 
 	def ShowContextMenu( self ) :
 		context = []
-		context.append( ContextItem( MR_LANG( 'Add favorite addon' ), CONTEXT_ADD_FAVORITE ) )
+		context.append( ContextItem( MR_LANG( 'Add favorite add-on' ), CONTEXT_ADD_FAVORITE ) )
 		if self.mFavoriteAddonsIdList and len( self.mFavoriteAddonsIdList ) > 0 and self.mSelectedIndex != -1 :
-			context.append( ContextItem( MR_LANG( 'Delete favorite addon' ), CONTEXT_DELETE_FAVORITE ) )
+			context.append( ContextItem( MR_LANG( 'Delete favorite add-on' ), CONTEXT_DELETE_FAVORITE ) )
 			context.append( ContextItem( MR_LANG( 'Run' ), CONTEXT_RUN_FAVORITE ) )
 		dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_CONTEXT )
 		dialog.SetProperty( context )
@@ -178,19 +178,19 @@ class FavoriteAddons( BaseWindow ) :
 			tmpList = xbmc.executehttpapi( "getaddons()" )
 			if tmpList == '<li>' :
 				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
-				dialog.SetDialogProperty( MR_LANG( 'Error' ), MR_LANG( 'No addons installed' ) )
+				dialog.SetDialogProperty( MR_LANG( 'Error' ), MR_LANG( 'No add-ons installed' ) )
 	 			dialog.doModal( )
 			else :
 				addonList = tmpList[4:].split( ':' )
 				dialog = xbmcgui.Dialog( )
-				ret = dialog.select( MR_LANG( 'Select Addon' ), addonList )
+				ret = dialog.select( MR_LANG( 'Select Add-on' ), addonList )
 				if ret >= 0 :
 					ret1 = xbmc.executehttpapi( "addfavourite(%s)" % addonList[ret] )
 					self.UpdateListItem( )
 
 		elif aContextAction == CONTEXT_DELETE_FAVORITE :
 			dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_YES_NO_CANCEL )
-			dialog.SetDialogProperty(  MR_LANG( 'Delete favorite addon' ),  MR_LANG( 'Do you want to remove %s?' ) % self.mFavoriteAddonsIdList[ self.mSelectedIndex ].getProperty( 'AddonId' ) )
+			dialog.SetDialogProperty(  MR_LANG( 'Delete favorite add-on' ),  MR_LANG( 'Do you want to remove %s?' ) % self.mFavoriteAddonsIdList[ self.mSelectedIndex ].getProperty( 'AddonId' ) )
 			dialog.doModal( )
 			if dialog.IsOK( ) == E_DIALOG_STATE_YES :
 				ret = xbmc.executehttpapi( "removefavourite(%s)" % self.mFavoriteAddonsIdList[ self.mSelectedIndex ].getProperty( 'AddonId' ) )
