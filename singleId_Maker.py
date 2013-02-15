b909e6546700c72bde962836b9cd4f25b0302159
bsyoun <bsyoun@marusys.com>
2013-02-15 12:26:07 +0900

    debug - idMaker

--- singleId_Maker.py
+++ singleId_Maker.py
@@ -2,16 +2,91 @@
 
 #from sys import setdefaultencoding
 #setdefaultencoding('utf-8')
-import os, re, shutil, time, sys
+import os, re, shutil, time, sys, glob
 import sgmllib, string
 from types import *
 
 gNoParseList = {}
-E_ID_LIVEPLATE = 50000
+E_DIR_RESULT = os.getcwd() + '/changeTest'
+
+E_ID_SINGLE_CONVERT = 50000
+
+E_IDS_SINGLE_WINDOW = {
+	'TunerConfiguration.xml' : 50000,
+	'ManualScan.xml' : None,
+	'EditSatellite.xml' : None,
+	'DialogMoveAntenna.xml' : None,
+	'SystemUpdate.xml' : None,
+	'FirstInstallation.xml' : None,
+	'ChannelSearch.xml' : None,
+	'Installation.xml' : None,
+	'TimeshiftPlate.xml' : None,
+	'DialogMultiSelect.xml' : None,
+	'DialogChannelSearch.xml' : None,
+	'DialogLnbFrequency.xml' : None,
+	'Help_String.xml' : None,
+	'MediaCenter.xml' : None,
+	'LivePlate.xml' : None,
+	'SystemInfo.xml' : None,
+	'AutomaticScan.xml' : None,
+	'AntennaSetup.xml' : None,
+	'DialogSetTransponder.xml' : None,
+	'DialogChannelJump.xml' : None,
+	'DialogAddNewSatellite.xml' : None,
+	'ArchiveWindow.xml' : None,
+	'DialogEditChannelList.xml' : None,
+	'SatelliteConfigMotorized12.xml' : None,
+	'DialogStartRecord.xml' : None,
+	'ChannelListWindow.xml' : None,
+	'EditTransponder.xml' : None,
+	'DialogYesNoCancel.xml' : None,
+	'DialogAddManualTimer.xml' : None,
+	'MainMenu.xml' : None,
+	'DialogStopRecord.xml' : None,
+	'ConditionalAccess.xml' : None,
+	'DialogNormalNumeric.xml' : None,
+	'SatelliteConfigSimple.xml' : None,
+	'DialogSatelliteNumeric.xml' : None,
+	'DialogPopupOK.xml' : None,
+	'DialogForceProgress.xml' : None,
+	'Favorites.xml' : None,
+	'DialogCasEvent.xml' : None,
+	'DialogExtendEPG.xml' : None,
+	'FavoriteAddons.xml' : None,
+	'DialogBookmark.xml' : None,
+	'Configure.xml' : None,
+	'DialogAutoPowerDown.xml' : None,
+	'DialogSetAudioVideo.xml' : None,
+	'DialogContext.xml' : None,
+	'SatelliteConfigDisEqC11.xml' : None,
+	'Help.xml' : None,
+	'EPGWindow.xml' : None,
+	'TimerWindow.xml' : None,
+	'SatelliteConfigOnecable.xml' : None,
+	'DialogAddTimer.xml' : None,
+	'NullWindow.xml' : None,
+	'SatelliteConfigMotorizedUsals.xml' : None,
+	'DialogInputPincode.xml' : None,
+	'SatelliteConfigOnecable2.xml' : None,
+	'SatelliteConfigDisEqC10.xml' : None
+	}
 
 E_ID_EXCEPTION = [
 	8800,
 	8899
+	]
+
+E_FILE_EXCEPTION = [
+	'rootwindow.xml',
+	'settings.xml',
+	'addon.xml',
+	'confluence_texture_cache.xml',
+	'confluence_texture_cache.xml',
+	'elmo_test.xml',
+	'mbox_includes.xml',
+	'hiddentest.xml',
+	'help_string.xml',
+	'loading.xml'
 	]
 
 E_PATTERN_STRINGS = [
@@ -22,6 +97,7 @@ E_PATTERN_STRINGS = [
 	'<onup>(.*)\d+(.*)</onup>',
 	'<ondown>(.*)\d+(.*)</ondown>',
 	'<onclick>\d+</onclick>',
+
 	#'<onleft>SetFocus\(\d+\)</onleft>',
 	#'<onright>SetFocus\(\d+\)</onright>',
 	#'<onup>SetFocus\(\d+\)</onup>',
@@ -31,37 +107,80 @@ E_PATTERN_STRINGS = [
 	'Container\(\d+\)\.'
 	]
 
-def AutoChangeControlIDs( ) :
-	currDir = os.getcwd()
-	mboxDir = os.path.abspath(currDir + '/../../../../script.mbox')
-	elisDir = os.path.abspath(currDir + '/../../../../script.module.elisinterface')
 
-	resultDir = os.getcwd() + '/changeTest'
-
+def InitDir( ) :
 	#init dir
 	try:
-		shutil.rmtree(resultDir)
+		shutil.rmtree( E_DIR_RESULT )
 		#os.remove(wFile1)
-	except Exception, e:
-		#print e
+	except Exception, e :
+		print e
 		pass
-	os.mkdir(resultDir, 0775)
+	os.mkdir( E_DIR_RESULT, 0775 )
 
 
-	confluenceDir = 'resources/skins/Default/720p/'
-	openFile = confluenceDir + 'LivePlate.xml'
-	changeFile = '%s/%s'% ( resultDir, 'LivePlate.xml' )
-	sourceXml = ''
+gCount = 0
+gName = None
+def FindallSource(aDir, patternStr, reqFile=None):
+	retlist = glob.glob(os.path.join(aDir, patternStr))
+	findlist = os.listdir(aDir)
+	global gCount, gName
+	searched = None
+
+	for fname in findlist:
+		next = os.path.join(aDir, fname)
+		if os.path.isdir(next) : 
+			retlist += FindallSource(next, patternStr, reqFile)
+		else :
+			if reqFile :
+				if fname == reqFile :
+					searched = '%s/%s'% ( aDir,fname )
+					gName = searched
+					break
+
+			else :
+				if gNoParseList.get( fname.lower() ) == fname.lower() :
+					#print gNoParseList.get(fname)
+					continue
+
+				filename = os.path.splitext( fname )
+				if filename[1] == '.xml' :
+					gCount += 1
+
+					global E_ID_SINGLE_CONVERT 
+					E_ID_SINGLE_CONVERT = E_IDS_SINGLE_WINDOW.get( fname, None )
+					if not E_ID_SINGLE_CONVERT :
+						E_ID_SINGLE_CONVERT = 100000 * gCount 
+
+					print '%s[%s]%s'% ( gCount, E_ID_SINGLE_CONVERT , fname )
+					ParseSource( '%s/%s'% (aDir, fname) )
+					#sys.exit( 11 )
+
+
+	return retlist
+
+
+gDebug = False
+def ParseSource( aSourceFile ) :
+	if not aSourceFile :
+		return
+
 	try:
-		rf = open( openFile, 'r' )
+		rf = open( aSourceFile, 'r' )
 		sourceXml = rf.readlines( )
 		rf.close( )
 
 	except Exception, e:
-		print 'Cannot open file[%s]'% openFile
+		print 'except[%s] Cannot open file[%s]'% ( e, aSourceFile )
 		return
 
-	fp = open(changeFile, 'w')
+	filename = os.path.basename( aSourceFile )
+	changeFile = '%s/%s'% ( E_DIR_RESULT, filename )
+	try :
+		fp = open(changeFile, 'w')
+	except Exception, e :
+		print 'except[%s] Cannot open file[%s]'% ( e, changeFile )
+		return
 
 	count = 0
 	countM= 0
@@ -79,7 +198,7 @@ def AutoChangeControlIDs( ) :
 					countM += 1
 				line = lineM
 
-		if isMatch :
+		if isMatch and gDebug :
 			print count, line
 
 		fp.writelines( '%s\n'% line )
@@ -92,14 +211,32 @@ def AutoChangeControlIDs( ) :
 
 def ChangeIds( aMatch ) :
 	idnumber = re.findall( '\d+', aMatch.group() )[0]
-	changeid = E_ID_LIVEPLATE + int( idnumber )
+	changeid = E_ID_SINGLE_CONVERT + int( idnumber )
 	#print '----id[%s -> %s] match[%s]'% ( idnumber, changeid, aMatch.group() )
 
-	value = re.sub( '\d+', '%s'% changeid, aMatch.group() )
+	value = re.sub( '\d+', '%s'% changeid, aMatch.group(), 1 )
 	if int( idnumber ) in E_ID_EXCEPTION :
 		value = aMatch.group()
 
 	return value
+
+
+def AutoMakeSingleIDs( ) :
+	currDir = os.getcwd()
+	mboxDir = os.path.abspath( currDir + '/../script.mbox' )
+	elisDir = os.path.abspath( currDir + '/../script.module.elisinterface' )
+	confluenceDir = os.path.join( mboxDir, 'resources/skins/Default/720p' )
+
+	for iFile in E_FILE_EXCEPTION :
+		gNoParseList[iFile] = iFile
+
+	InitDir( )
+	#openFile = confluenceDir + 'LivePlate.xml'
+	#ParseSource( openFile )
+	FindallSource( confluenceDir, '[a-zA-Z0-9]\w*.xml' )
+	print '\nChangeAll *.xml files[%s]'% gCount
+	print '\n\033[1;%sm%s\033[1;m'% (30, 'Completed..')
+
 
 
 def test( ) :
@@ -108,7 +245,8 @@ def test( ) :
 	#strs = '<animation effect="slide" start="-200,0" end="0,0" delay="0" time="300" condition="ControlGroup(3620).HasFocus) | Control.HasFocus(702) | Control.HasFocus(706)">conditional</animation>'
 	#strs = '<defaultcontrol always="true">3621</defaultcontrol>'
 	#strs = '<defaultcontrol>2222</defaultcontrol>'
-	strs = '<onup>Control.SetFocus(102,4)</onup>'
+	#strs = '<onup>Control.SetFocus(102,4)</onup>'
+	strs = '<ondown condition="Control.IsVisible(9001)">9001</ondown>'
 
 	for pattern in E_PATTERN_STRINGS :
 		ret = re.findall( pattern, strs, re.I )
@@ -125,15 +263,13 @@ if __name__ == "__main__":
 	gNoParseList[nameSelf] = nameSelf
 	param = sys.argv[1:]
 
-	"""
-	if len(param) > 0 :
-		if param[0] == 'csv' :
-			print 'create new csv : test.csv'
-	else :
-		test( )
-	"""
 
-	#AutoChangeControlIDs( )
-	test( )
+	if len(param) > 0 :
+		if param[0] == 'debug' :
+			global gDebug
+			gDebug = True
+
+	AutoMakeSingleIDs( )
+	#test( )
 
 
