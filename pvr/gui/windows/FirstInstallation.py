c9d24da71043eab6977d9b066776a3801d4d0add
bsyoun <bsyoun@marusys.com>
2013-04-09 22:13:26 +0900

    debug - 1080p front resolution

--- pvr/gui/windows/FirstInstallation.py
+++ pvr/gui/windows/FirstInstallation.py
@@ -459,7 +459,11 @@ class FirstInstallation( FTIWindow ) :
 		if hdmiFormat == 'Automatic' :
 			return
 		iconIndex = ElisEnum.E_ICON_1080i
-		if hdmiFormat == '720p' :
+		if hdmiFormat == '1080p-50' :
+			iconIndex = ElisEnum.E_ICON_1080p
+		elif hdmiFormat == '1080p-25' :
+			iconIndex = ElisEnum.E_ICON_1080p
+		elif hdmiFormat == '720p' :
 			iconIndex = ElisEnum.E_ICON_720p
 		elif hdmiFormat == '576p' :
 			iconIndex = -1
