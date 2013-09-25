a598879c6d41949b01d8b835621d067313471c06
donghwakim <donghwakim@marusys.com>
2013-09-25 18:34:01 +0900

    add : fast scan operation

--- pvr/gui/windows/FastScan.py
+++ pvr/gui/windows/FastScan.py
@@ -1,14 +1,22 @@
 from pvr.gui.WindowImport import *
+from elementtree import ElementTree
 
-E_FAST_SCAN_BASE_ID = WinMgr.WIN_ID_FAST_SCAN * E_BASE_WINDOW_UNIT + E_BASE_WINDOW_ID 
+E_FAST_SCAN_BASE_ID = WinMgr.WIN_ID_FAST_SCAN * E_BASE_WINDOW_UNIT + E_BASE_WINDOW_ID
+
+FILE_PROVIDER	= '/config/Provider.xml'
 
 
 class FastScan( SettingWindow ) :
 	def __init__( self, *args, **kwargs ) :
 		SettingWindow.__init__( self, *args, **kwargs )
-		#self.mSatelliteIndex = 0
-		#self.mFormattedList = None
-		#self.mConfiguredSatelliteList = None
+		self.mProviderStruct	= ElisFastScanProviderInfo( )
+		
+		self.mTunerIndex		= E_TUNER_1
+		self.mUseHDList			= 0
+		self.mUseNumbering		= 0
+		self.mUseNaming			= 0
+
+		self.mOPDescr			= 'None'
 
 
 	def onInit( self ) :
@@ -56,14 +64,55 @@ class FastScan( SettingWindow ) :
 		if self.IsActivate( ) == False  :
 			return
 	
-		#groupId = self.GetGroupId( aControlId )
+		groupId = self.GetGroupId( aControlId )
 		
-		# Satellite
-		#if groupId == E_Input01 :
+		# TunerIndex
+		if groupId == E_Input01 :
+			dialog = xbmcgui.Dialog( )
+			ret = dialog.select( MR_LANG( 'Select Tuner' ), [ MR_LANG( 'Tuner 1' ), MR_LANG( 'Tuner 2' ) ], False, self.mTunerIndex )
+			if ret >= 0 and self.mTunerIndex != ret :
+				self.mTunerIndex = ret
+				self.SetControlLabel2String( E_Input01, self.GetTunerNumToString( self.mTunerIndex ) )
+
+		# Provider
+		if groupId == E_Input02 :
+			providerList = self.GetProviderList( )
+			if providerList :
+				dialog = xbmcgui.Dialog( )
+				ret = dialog.select( MR_LANG( 'Select Provider' ), providerList, False, StringToListIndex( providerList, self.mOPDescr ) )
+				if ret >= 0 :
+					if self.SetProviderInfo( ret ) :
+						self.SetControlLabel2String( E_Input02, self.mOPDescr )
+						self.InitConfig( )
+			else :
+				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
+				dialog.SetDialogProperty( MR_LANG( 'Error' ), MR_LANG( 'Provider file error' ), MR_LANG( 'Check a provider file first' ) )
+				dialog.doModal( )
 			
-		
-		#elif groupId == E_SpinEx01 or groupId == E_SpinEx02 or groupId == E_SpinEx03 :
-		#	self.ControlSelect( )
+
+		# Start Scan
+		if groupId == E_Input03 :
+			if self.mProviderStruct :
+				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_CHANNEL_SEARCH )
+				providerlist = []
+				temp = deepcopy( self.mProviderStruct )
+				temp.mHasHDList = self.mUseHDList
+				providerlist.append( temp )
+				dialog.SetProvider( self.mTunerIndex, self.mUseNumbering, self.mUseNaming, providerlist )
+				dialog.doModal( )
+			else :
+				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
+				dialog.SetDialogProperty( MR_LANG( 'Error' ), MR_LANG( 'No provider info available' ), MR_LANG( 'Select provider first' ) )
+				dialog.doModal( )
+
+		elif groupId == E_SpinEx01 :
+			self.mUseHDList = self.GetSelectedIndex( E_SpinEx01 )
+
+		elif groupId == E_SpinEx02 :
+			self.mUseNumbering = self.GetSelectedIndex( E_SpinEx02 )
+
+		elif groupId == E_SpinEx03 :
+			self.mUseNaming = self.GetSelectedIndex( E_SpinEx03 )
 
 
 	def onFocus( self, aControlId ) :
@@ -75,12 +124,153 @@ class FastScan( SettingWindow ) :
 
 
 	def InitConfig( self ) :
-		#self.ResetAllControl( )
-		self.AddInputControl( E_Input01, MR_LANG( 'Tuner' ), 'Tuner 1', MR_LANG( 'Select Tuner' ) )
-		self.AddInputControl( E_Input02, MR_LANG( 'Provider' ), 'marusys', MR_LANG( 'Select Provider' ) )
-		self.AddUserEnumControl( E_SpinEx01, MR_LANG( 'HD List' ), USER_ENUM_LIST_YES_NO, 0, MR_LANG( 'HD LIST' ) )
-		self.AddUserEnumControl( E_SpinEx02, MR_LANG( 'Use fastscan channel numbering' ), USER_ENUM_LIST_YES_NO, 0, MR_LANG( 'Use fastscan channel numbering?' ) )
-		self.AddUserEnumControl( E_SpinEx03, MR_LANG( 'Use fastscan channel names' ), USER_ENUM_LIST_YES_NO, 0, MR_LANG( 'Use fastscan channel names?' ) )
+		self.ResetAllControl( )
+		self.GetTunerNumToString( self.mTunerIndex )
+		self.AddInputControl( E_Input01, MR_LANG( 'Tuner' ), self.GetTunerNumToString( self.mTunerIndex ), MR_LANG( 'Select Tuner' ) )
+		self.AddInputControl( E_Input02, MR_LANG( 'Provider' ), self.mOPDescr, MR_LANG( 'Select Provider' ) )
+		self.AddUserEnumControl( E_SpinEx01, MR_LANG( 'HD List' ), USER_ENUM_LIST_YES_NO, self.mUseHDList, MR_LANG( 'HD LIST' ) )
+		self.AddUserEnumControl( E_SpinEx02, MR_LANG( 'Use fastscan channel numbering' ), USER_ENUM_LIST_YES_NO, self.mUseNumbering, MR_LANG( 'Use fastscan channel numbering?' ) )
+		self.AddUserEnumControl( E_SpinEx03, MR_LANG( 'Use fastscan channel names' ), USER_ENUM_LIST_YES_NO, self.mUseNaming, MR_LANG( 'Use fastscan channel names?' ) )
 		self.AddInputControl( E_Input03, MR_LANG( 'Start Search' ), '', MR_LANG( 'Press OK button to start a channel search' ) )
 		self.InitControl( )
+		self.DisableControl( )
 
+
+	def GetProviderList( self ) :
+		if not os.path.exists( FILE_PROVIDER ) :
+			return None
+
+		try :
+			tree = ElementTree.parse( FILE_PROVIDER )
+			root = tree.getroot( )
+			nameList = []
+
+			for provider in root.findall( 'Provider' ) :
+				for name in provider.findall( 'OPDescription' ) :
+					#print 'dhkim test OPDescription = %s' % name.text
+					nameList.append( name.text.encode( 'utf-8' ) )
+
+			if len( nameList ) > 0 :
+				return nameList
+			else :
+				return None
+
+		except Exception, e :
+			LOG_ERR( 'Error exception[%s]' % e )
+			return None
+
+
+	def SetProviderInfo( self, aIndex ) :
+		if not os.path.exists( FILE_PROVIDER ) :
+			return False
+
+		try :
+			tree = ElementTree.parse( FILE_PROVIDER )
+			root = tree.getroot( )
+
+			provider = root.getchildren( )[ aIndex ]
+
+			self.mOPDescr = provider.find( 'OPDescription' ).text.encode( 'utf-8' )
+			self.mUseHDList = self.GetHDListEnum( provider.find( 'HDList' ).text.encode( 'utf-8' ) )
+
+			struct = ElisFastScanProviderInfo( )
+			struct.reset( )
+			struct.mName			= provider.find( 'SatelliteName' ).text.encode( 'utf-8' )
+			struct.mLongitude		= int( provider.find( 'SatelliteLongitude' ).text.encode( 'utf-8' ) )
+			struct.mBand			= self.GetBandEnum( provider.find( 'SatelliteBand' ).text.encode( 'utf-8' ) )
+			struct.mTransponderId	= int( provider.find( 'TransponderId' ).text.encode( 'utf-8' ) )
+			struct.mFrequency		= int( provider.find( 'Frequency' ).text.encode( 'utf-8' ) )
+			struct.mSymbolRate		= int( provider.find( 'Symbolrate' ).text.encode( 'utf-8' ) )
+			struct.mPolarization	= self.GetPolEnum( provider.find( 'Polarization' ).text.encode( 'utf-8' ) )
+			struct.mFEC				= self.GetFECEnum( provider.find( 'FECMode' ).text.encode( 'utf-8' ) )
+			struct.mPid				= int( provider.find( 'Pid' ).text.encode( 'utf-8' ) )
+			struct.mPidDescr		= provider.find( 'PidDescription' ).text.encode( 'utf-8' )
+			struct.mOPIndetifier	= int( provider.find( 'OPIndetifier' ).text.encode( 'utf-8' ) )
+			struct.mOPDescr			= provider.find( 'OPDescription' ).text.encode( 'utf-8' )
+			struct.mHasHDList		= self.GetHDListEnum( provider.find( 'HDList' ).text.encode( 'utf-8' ) )
+
+			self.mProviderStruct = struct
+			self.mProviderStruct.printdebug( )
+			return True
+
+		except Exception, e :
+			LOG_ERR( 'Error exception[%s]' % e )
+			return False
+
+
+	def GetBandEnum( self, aBand ) :
+		if aBand == 'BAND_KU' :
+			return ElisEnum.E_BAND_KU
+		elif aBand == 'BAND_C' :
+			return ElisEnum.E_BAND_C
+		else :
+			return ElisEnum.E_BAND_UNDEFINED
+
+
+	def GetPolEnum( self, aPol ) :
+		if aPol == 'LNB_HORIZONTAL' :
+			return ElisEnum.E_LNB_HORIZONTAL
+		else :
+			return ElisEnum.E_LNB_VERTICAL
+
+
+	def GetFECEnum( self, aFEC ) :
+		if aFEC == 'DVBS_1_2' :
+			return ElisEnum.E_DVBS_1_2
+		elif aFEC == 'DVBS_2_3' :
+			return ElisEnum.E_DVBS_2_3
+		elif aFEC == 'DVBS_3_4' :
+			return ElisEnum.E_DVBS_3_4
+		elif aFEC == 'DVBS_5_6' :
+			return ElisEnum.E_DVBS_5_6
+		elif aFEC == 'DVBS_7_8' :
+			return ElisEnum.E_DVBS_7_8
+		elif aFEC == 'DVBS2_QPSK_1_2' :
+			return ElisEnum.E_DVBS2_QPSK_1_2
+		elif aFEC == 'DVBS2_QPSK_3_5' :
+			return ElisEnum.E_DVBS2_QPSK_3_5
+		elif aFEC == 'DVBS2_QPSK_2_3' :
+			return ElisEnum.E_DVBS2_QPSK_2_3
+		elif aFEC == 'DVBS2_QPSK_3_4' :
+			return ElisEnum.E_DVBS2_QPSK_3_4
+		elif aFEC == 'DVBS2_QPSK_4_5' :
+			return ElisEnum.E_DVBS2_QPSK_4_5
+		elif aFEC == 'DVBS2_QPSK_5_6' :
+			return ElisEnum.E_DVBS2_QPSK_5_6
+		elif aFEC == 'DVBS2_QPSK_8_9' :
+			return ElisEnum.E_DVBS2_QPSK_8_9
+		elif aFEC == 'DVBS2_QPSK_9_10' :
+			return ElisEnum.E_DVBS2_QPSK_9_10
+		elif aFEC == 'DVBS2_8PSK_3_5' :
+			return ElisEnum.E_DVBS2_8PSK_3_5
+		elif aFEC == 'DVBS2_8PSK_2_3' :
+			return ElisEnum.E_DVBS2_8PSK_2_3
+		elif aFEC == 'DVBS2_8PSK_3_4' :
+			return ElisEnum.E_DVBS2_8PSK_3_4
+		elif aFEC == 'DVBS2_8PSK_5_6' :
+			return ElisEnum.E_DVBS2_8PSK_5_6
+		elif aFEC == 'DVBS2_8PSK_8_9' :
+			return ElisEnum.E_DVBS2_8PSK_8_9
+		elif aFEC == 'DVBS2_8PSK_9_10' :
+			return ElisEnum.E_DVBS2_8PSK_9_10
+
+
+	def GetHDListEnum( self, aHDList ) :
+		if aHDList == 'true' :
+			return 1
+		else :
+			return 0
+
+
+	def GetTunerNumToString( self, aTunerIndex ) :
+		if aTunerIndex == E_TUNER_1 :
+			return MR_LANG( 'Tuner 1' )
+		else :
+			return MR_LANG( 'Tuner 2' )
+
+
+	def DisableControl( self ) :
+		if self.mProviderStruct.mHasHDList == 0 :
+			self.SetEnableControl( E_SpinEx01, False )
+		else :
+			self.SetEnableControl( E_SpinEx01, True )
