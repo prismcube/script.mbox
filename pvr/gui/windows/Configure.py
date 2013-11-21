c73e125184a43a28c1f9e373f43740ca0b375fdd
bsyoun <bsyoun@marusys.com>
2013-11-21 19:56:36 +0900

    add restart system

--- pvr/gui/windows/Configure.py
+++ pvr/gui/windows/Configure.py
@@ -493,6 +493,13 @@ class Configure( SettingWindow ) :
 			dialog.doModal( )
 
 			if dialog.IsOK( ) == E_DIALOG_STATE_YES :
+				isDownload = WinMgr.GetInstance( ).GetWindow( WinMgr.WIN_ID_SYSTEM_UPDATE ).GetStatusFromFirmware( )
+				if isDownload :
+					dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
+					dialog.SetDialogProperty( MR_LANG( 'Attention' ), MR_LANG( 'Try again after completing firmware update' ) )
+					dialog.doModal( )
+					return
+
 				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
 				dialog.SetDialogProperty( MR_LANG( 'Restart Required' ), MR_LANG( 'Your system must be restarted%s in order to complete the XBMC reset' ) % NEW_LINE )
 	 			dialog.doModal( )
