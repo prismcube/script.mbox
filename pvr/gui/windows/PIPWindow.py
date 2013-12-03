9286cccfb84d2e43f26cd25b603eed50881e1c70
bsyoun <bsyoun@marusys.com>
2013-12-03 19:03:00 +0900

    debug - changed ui

--- pvr/gui/windows/PIPWindow.py
+++ pvr/gui/windows/PIPWindow.py
@@ -63,7 +63,8 @@ PIP_CHECKWINDOW = [
 	WinMgr.WIN_ID_NULLWINDOW,
 	WinMgr.WIN_ID_MAINMENU,
 	WinMgr.WIN_ID_TIMESHIFT_PLATE,
-	WinMgr.WIN_ID_LIVE_PLATE
+	WinMgr.WIN_ID_LIVE_PLATE,
+	WinMgr.WIN_ID_PIP_WINDOW
 ]
 
 
@@ -255,7 +256,7 @@ class PIPWindow( BaseWindow ) :
 		#self.mEventBus.Deregister( self )
 
 		self.setFocusId( CTRL_ID_BUTTON_NEXT_PIP )
-		self.UpdatePropertyGUI( 'ShowFocusPIP', E_TAG_FALSE )
+		self.UpdatePropertyGUI( 'ShowNamePIP', E_TAG_FALSE )
 
 		if self.mPIP_EnableAudio :
 			self.mDataCache.PIP_EnableAudio( False )
@@ -300,7 +301,7 @@ class PIPWindow( BaseWindow ) :
 
 			ret = self.mDataCache.PIP_AVBlank( not isShow )
 			self.UpdatePropertyGUI( 'OpenPIP', '%s'% isShow )
-			self.UpdatePropertyGUI( 'ShowNamePIP', '%s'% isShow )
+			#self.UpdatePropertyGUI( 'ShowNamePIP', '%s'% isShow )
 			LOG_TRACE( 'GetLastWindowID[%s] isPIPShow[%s]'% ( WinMgr.GetInstance( ).GetLastWindowID( ), isShow ) )
 
 		LOG_TRACE( 'mPIPStart[%s] OpenPIP[%s]'% ( self.mPIPStart, self.getProperty( 'OpenPIP' ) ) )
@@ -311,8 +312,6 @@ class PIPWindow( BaseWindow ) :
 
 
 	def Load( self ) :
-		self.ResetLabel( )
-
 		"""
 		self.mIndexLimit = 1
 		numList = self.mDataCache.PIP_GetTunableList( )
@@ -320,6 +319,7 @@ class PIPWindow( BaseWindow ) :
 			self.mIndexLimit = len( numList )
 		"""
 
+		self.ResetLabel( )
 		ret = self.ChannelTuneToPIP( CURR_CHANNEL_PIP )
 		if ret :
 			self.LoadPositionPIP( )
@@ -424,7 +424,7 @@ class PIPWindow( BaseWindow ) :
 
 	def ChannelTuneToPIP( self, aDir ) :
 		fakeChannel = self.mCurrentChannel
-		if aDir != INPUT_CHANNEL_PIP :
+		if aDir != INPUT_CHANNEL_PIP and aDir != CURR_CHANNEL_PIP :
 			self.UpdatePropertyGUI( 'BlankPIP', E_TAG_TRUE )
 
 		if not fakeChannel or fakeChannel.mError != 0 or fakeChannel.mNumber == 0 :
@@ -483,7 +483,7 @@ class PIPWindow( BaseWindow ) :
 		elif aDir == CURR_CHANNEL_PIP :
 			if self.mPIPStart :
 				LOG_TRACE( '--------Already started PIP' )
-				self.mDataCache.PIP_AVBlank( False )
+				#self.mDataCache.PIP_AVBlank( False )
 
 				if self.mCurrentChannel and ( not self.mCurrentChannel.mLocked ) and \
 				   self.getProperty( 'PIPSignal' ) == E_TAG_TRUE :
@@ -491,6 +491,7 @@ class PIPWindow( BaseWindow ) :
 
 				return True
 
+			self.UpdatePropertyGUI( 'BlankPIP', E_TAG_TRUE )
 			if not fakeChannel :
 				self.Close( )
 				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
@@ -503,14 +504,13 @@ class PIPWindow( BaseWindow ) :
 		LOG_TRACE( '---------up/down[%s] fakeChannel[%s] current[%s %s]'% ( aDir, fakeChannel, self.mCurrentChannel.mNumber, self.mCurrentChannel.mName ) )
 		if fakeChannel :
 			LOG_TRACE('----------fakeChannel[%s %s]'% ( fakeChannel.mNumber, fakeChannel.mName ) )
-			label = 'PIP - %s'% fakeChannel.mName
-			self.mCtrlLabelChannel.setLabel( label )
-
 			pChNumber = fakeChannel.mNumber
 			if E_V1_2_APPLY_PRESENTATION_NUMBER :
 				pChNumber = self.mDataCache.CheckPresentationNumber( fakeChannel )
 
-			self.UpdatePropertyGUI( 'ShowPIPChannelNumber', '%s'% pChNumber ) 
+			label = '%s - %s'% ( pChNumber, fakeChannel.mName )
+			self.mCtrlLabelChannel.setLabel( label )
+			#self.UpdatePropertyGUI( 'ShowPIPChannelNumber', '%s'% pChNumber ) 
 
 			self.mFakeChannel = fakeChannel
 			self.RestartAsyncTune( )
@@ -519,6 +519,11 @@ class PIPWindow( BaseWindow ) :
 
 
 	def SetLabelInputNumber( self ) :
+		imagePng = 'black-back_noAlpha.png'
+		if self.getProperty( 'PIPSignal' ) != E_TAG_TRUE :
+			imagePng = 'button-focus2.png'
+
+		self.mCtrlImageInputBG.setImage( imagePng )
 		self.UpdatePropertyGUI( 'InputNumber', E_TAG_TRUE )
 		self.mCtrlLabelInputCH.setLabel( self.mInputString )
 
