7bc232ec08180603380c0cd74b38380710c281a8
bsyoun <bsyoun@marusys.com>
2013-12-05 13:28:10 +0900

    debug - block restart system when firmware downloading

--- pvr/gui/windows/MainMenu.py
+++ pvr/gui/windows/MainMenu.py
@@ -202,13 +202,6 @@ class MainMenu( BaseWindow ) :
 			self.GoToEditChannelList( )
 
 		elif aControlId == BUTTON_ID_POWER :
-			isDownload = WinMgr.GetInstance( ).GetWindow( WinMgr.WIN_ID_SYSTEM_UPDATE ).GetStatusFromFirmware( )
-			if isDownload :
-				msg = MR_LANG( 'Try again after completing firmware update' )
-				dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
-				dialog.SetDialogProperty( MR_LANG( 'Attention' ), msg )
-				dialog.doModal( )
-				return
 
 			context = []
 			context.append( ContextItem( MR_LANG( 'Active Standby' ), 1 ) )
@@ -233,6 +226,14 @@ class MainMenu( BaseWindow ) :
 			elif contextAction == 2 :
 				self.mCommander.System_StandbyMode( 0 )
 			elif contextAction == 3 :
+				isDownload = WinMgr.GetInstance( ).GetWindow( WinMgr.WIN_ID_SYSTEM_UPDATE ).GetStatusFromFirmware( )
+				if isDownload :
+					msg = MR_LANG( 'Try again after completing firmware update' )
+					dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_POPUP_OK )
+					dialog.SetDialogProperty( MR_LANG( 'Attention' ), msg )
+					dialog.doModal( )
+					return
+
 				self.mDataCache.System_Reboot( )
 
 		elif ( aControlId >= BUTTON_ID_MEDIA_CENTER and aControlId <= BUTTON_ID_MEDIA_SYS_INFO ) or aControlId == BUTTON_ID_FAVORITE_EXTRA  :
