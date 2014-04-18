05e0f6b6ac783dcfdf0b2657b7854a2ac78875dd
daniel <danielchang@marusys.com>
2014-04-18 13:42:38 +0900

    Updated Slovak and Czech translation

--- pvr/DataCacheMgr.py
+++ pvr/DataCacheMgr.py
@@ -2322,7 +2322,7 @@ class DataCacheMgr( object ) :
 
 		retVal = 0
 		isFail = False
-		lblLine = MR_LANG( 'Network Volume failure' )
+		lblLine = MR_LANG( 'Network volume failure' )
 		try :
 			if not CheckNetworkStatus( ) :
 				retVal = -1
