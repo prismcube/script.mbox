98d75b5cd63a530793d10fb0425135be9a5dbbf2
John Kim <johnkim@marusys.com>
2014-07-02 18:06:46 +0900

    - channel list fetch speed upgraded

--- webinterface/getallservices.py
+++ webinterface/getallservices.py
@@ -3,6 +3,7 @@ from webinterface import Webinterface
 import dbopen
 from urllib import unquote, quote
 from xml.sax.saxutils import escape
+import time
 
 class ElmoGetAllServices( Webinterface ) :
 
@@ -13,10 +14,15 @@ class ElmoGetAllServices( Webinterface ) :
 
 		self.conn = dbopen.DbOpen('channel.db').getConnection()		
 		sql = "select Max(name) as name, sid, tsid, onid, Max(Presentation) as chno, ServiceType from tblChannel group by sid, tsid, onid order by chno"
-		
+
+		startTime = time.time()
+
 		self.c = self.conn.cursor()
 		self.c.execute(sql)
 		self.result = self.c.fetchall()
+
+		elapsedTime = time.time() - startTime
+		print str(elapsedTime)
 
 		#self.c.execute(sql)
 		#self.resultSub = self.c.fetchall()
@@ -32,24 +38,41 @@ class ElmoGetAllServices( Webinterface ) :
 			serviceList += '</e2service>\n'
 		serviceList += '</e2servicelist>\n' 
 		"""
+
+		print 'XML STRING CREATION STARTING AT ' + str(time.time()) 
 		
-		xmlStr = ''
-		xmlStr += '<?xml version="1.0" encoding="UTF-8"?>\n'
+		xmlStr = '<?xml version="1.0" encoding="UTF-8"?>\n'
 		xmlStr += '<e2servicelistrecursive>\n'
 
+		subXmlStr = []
+		
 		for row in self.result :
 		# def makeRef( self, sid, tsid, onid ) :
 
+			"""
 			#xmlStr += '<e2bouquet>\n'
 			xmlStr += '	<e2service>\n'
 			# xmlStr += '		<e2servicereference>' + self.makeRef(row[1], row[2], row[3]) + '</e2servicereference>\n'
 			xmlStr += '		<e2servicereference>' + self.makeRefServiceType(row[5], row[1], row[2], row[3]) + '</e2servicereference>\n'
-			xmlStr += '		<e2servicename>' + escape( row[0] ) + '</e2servicename>\n'
+			# xmlStr += '		<e2servicename>' + row[0].replace("&", " ") + '</e2servicename>\n'
+			xmlStr += '		<e2servicename>%s</e2servicename>\n' % escape(row[0])
 			xmlStr += '	</e2service>\n'
 			# xmlStr += serviceList
 			# xmlStr += '</e2bouquet>\n'	
+			"""
 
+			temp = """
+			<e2service>\n
+			<e2servicereference>%s</e2servicereference>\n
+			<e2servicename>%s</e2servicename>\n
+			</e2service>\n
+			""" % ( self.makeRefServiceType(row[5], row[1], row[2], row[3]), escape(row[0]) )
+			subXmlStr.append( temp )
+			
+		xmlStr += ''.join(subXmlStr)
 		xmlStr += '</e2servicelistrecursive>\n'
+
+		print 'XML STRING CREATION ENDS AT ' + str(time.time()) 
 		
 		return xmlStr
 		
