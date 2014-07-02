0030c387f423b4b1262314b1e06e0a5cee129588
daniel <danielchang@marusys.com>
2014-07-02 20:02:41 +0900

    edit : remove new line

--- pvr/gui/windows/ChannelListWindow.py
+++ pvr/gui/windows/ChannelListWindow.py
@@ -2343,7 +2343,7 @@ class ChannelListWindow( BaseWindow ) :
 				if self.mShowEPGInfo and self.mViewMode != WinMgr.WIN_ID_CHANNEL_EDIT_WINDOW :
 					epgEvent = self.GetEPGByIds( iChannel.mSid, iChannel.mTsid, iChannel.mOnid )
 					if epgEvent :
-						channelName = '%s %s(%s)%s'% ( iChannel.mName, E_TAG_COLOR_GREEN, epgEvent.mEventName, E_TAG_COLOR_END )
+						channelName = '%s %s(%s)%s'% ( iChannel.mName, E_TAG_COLOR_YELLOW, epgEvent.mEventName, E_TAG_COLOR_END )
 
 				listItem = xbmcgui.ListItem( '%04d'% iChNumber, '%s %s'% ( channelName, hdLabel ) )
 				if len( iChannel.mName ) > 30 :
@@ -2726,7 +2726,7 @@ class ChannelListWindow( BaseWindow ) :
 						channelName = '%s'% self.mNavChannel.mName
 						epgEvent = self.GetEPGByIds( self.mNavChannel.mSid, self.mNavChannel.mTsid, self.mNavChannel.mOnid )
 						if epgEvent :
-							channelName = '%s %s(%s)%s'% ( self.mNavChannel.mName, E_TAG_COLOR_GREEN, epgEvent.mEventName, E_TAG_COLOR_END )
+							channelName = '%s %s(%s)%s'% ( self.mNavChannel.mName, E_TAG_COLOR_YELLOW, epgEvent.mEventName, E_TAG_COLOR_END )
 
 						iChNumber = self.mNavChannel.mNumber
 						if E_V1_2_APPLY_PRESENTATION_NUMBER :
@@ -2823,7 +2823,7 @@ class ChannelListWindow( BaseWindow ) :
 					if self.mShowEPGInfo :
 						epgEvent = self.GetEPGByIds( iChannel.mSid, iChannel.mTsid, iChannel.mOnid )
 						if epgEvent :
-							channelName = '%s %s(%s)%s'% ( iChannel.mName, E_TAG_COLOR_GREEN, epgEvent.mEventName, E_TAG_COLOR_END )
+							channelName = '%s %s(%s)%s'% ( iChannel.mName, E_TAG_COLOR_YELLOW, epgEvent.mEventName, E_TAG_COLOR_END )
 							listItem.setProperty( 'percent', '%s'% self.GetEPGDurationProgress( epgEvent.mStartTime, epgEvent.mDuration ) )
 							listItem.setProperty( 'iPos', E_TAG_TRUE )
 						else :
