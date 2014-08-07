258cc99dab7232a698644860fa8d6979a3d461e3
HJ KIM <hjkim@marusys.com>
2014-08-07 15:55:23 +0900

    remove backup

--- pvr/gui/windows/ExclusiveSettings.py
+++ pvr/gui/windows/ExclusiveSettings.py
@@ -227,6 +227,7 @@ class ExclusiveSettings( object ) :
 
 		#1. check .xbmc ?
 		#if CheckMountType( aMntPath ).lower( ) == 'ext4' :
+		"""
 		if CheckDirectory( xbmcPath ) :
 			dialog = DiaMgr.GetInstance( ).GetDialog( DiaMgr.DIALOG_ID_YES_NO_CANCEL )
 			dialog.SetDialogProperty( MR_LANG( 'Backup data?' ), MR_LANG( 'To backup your user data and XBMC add-ons,%s insert a USB flash memory' ) % NEW_LINE )
@@ -237,7 +238,7 @@ class ExclusiveSettings( object ) :
 
 			elif ret == E_DIALOG_STATE_CANCEL :
 				return E_STORAGE_ERROR_FORMAT_CANCEL
-
+		"""
 		#ToDO : force dedicated format reboot ? <-- factory empty ?
 
 		#2. backup xbmc data
