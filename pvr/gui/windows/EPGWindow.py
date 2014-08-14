9ceee23e8a3293b08d88c2b49b4267f16b42dcbb
HJ KIM <hjkim@marusys.com>
2014-08-14 17:46:50 +0900

    Second running recording is not red color on EPGWindow

--- pvr/gui/windows/EPGWindow.py
+++ pvr/gui/windows/EPGWindow.py
@@ -2257,7 +2257,7 @@ class EPGWindow( BaseWindow ) :
 						weekStart = weekTimer.mStartTime + currentWeekStart
 						if weekStart > self.mDataCache.Datetime_GetLocalTime( ) :
 							isRunning = False
-			break
+				break
 
 		return isRunning
 			
